b15357895f4cee4d69b34f91a73e7a36630d8167
knappsk <109874645+knappsk@users.noreply.github.com>
2022-12-04 20:26:57 +0100

    Delete unnecessary Files

--- data_cleaning.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import numpy as np 
-
-metadata = pd.read_csv("../Project-Disney-World-DSF/metadata.csv")
-print(metadata)
-
-hauntedhouse = pd.read_csv("../Project-Disney-World-DSF/haunted_mansion[87].csv")
-print(hauntedhouse)
